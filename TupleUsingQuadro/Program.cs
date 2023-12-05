f07f07a300563c0c5b7982cd5079529739dced7e
Ruslan <ruslan.hilinsky@yandex.ru>
2023-12-05 04:22:54 +0300

    Задание на использование кортежей

---
+++ TupleUsingQuadro/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleUsingQuadro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the coefficients of the quadratic equation (a, b, c):");
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double c = double.Parse(Console.ReadLine());
+
+            var result = QuadraticEquation.CalculateRoots(a, b, c);
+
+            if (result.HasRoots)
+            {
+                Console.WriteLine($"Roots of the equation with coefficients a = {a}, b = {b}, c = {c} are different: x1 = {result.Root1}, x2 = {result.Root2}");
+            }
+            else if (result.HasOneRoot)
+            {
+                Console.WriteLine($"The root of the equation with coefficients a = {a}, b = {b}, c = {c} is one: x1 = x2 = {result.Root1}");
+            }
+            else
+            {
+                Console.WriteLine($"There are no roots of the equation with coefficients a = {a}, b = {b}, c = {c}.");
+            }
+        }
+    }
+}
---
+++ TupleUsingQuadro/QuadraticEquation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TupleUsingQuadro
+{
+    internal class QuadraticEquation
+    {
+        public static (bool HasRoots, bool HasOneRoot, double Root1, double Root2) CalculateRoots(double a, double b, double c)
+        {
+            double discriminant = b * b - 4 * a * c;
+
+            if (discriminant > 0)
+            {
+                double sqrtDiscriminant = Math.Sqrt(discriminant);
+                double x1 = (-b + sqrtDiscriminant) / (2 * a);
+                double x2 = (-b - sqrtDiscriminant) / (2 * a);
+                return (true, false, x1, x2);
+            }
+            else if (discriminant == 0)
+            {
+                double x = -b / (2 * a);
+                return (false, true, x, x);
+            }
+            else
+            {
+                return (false, false, 0, 0);
+            }
+        }
+    }
+}
